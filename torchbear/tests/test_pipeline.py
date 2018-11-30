c05ac92356b9f9e3762d4d189d203e74652a1ff1
Aaron Bentley <aaron@aaronbentley.com>
2018-11-29 21:17:14 -0500

    Revert to DependentTarget for better event-drivenness.

--- torchbear/tests/test_pipeline.py
+++ torchbear/tests/test_pipeline.py
@@ -8,7 +8,6 @@ from torchbear.event import (
     Queue,
     )
 from torchbear.pipeline import (
-    CoRoutineTarget,
     DependentTarget,
     Pipeline,
     Target,
@@ -117,103 +116,6 @@ class TestDependentTarget(TestCase):
 
         foo_target = DependentTarget('foo', [write_foo], [])
         bar_target = DependentTarget('bar', [write_bar], [foo_target])
-        pipeline = Pipeline([bar_target, foo_target], bar_target)
-        Listener(Queue()).run_pipeline(pipeline)
-        self.assertEqual('foobar', output.getvalue())
-
-
-class TestCoRoutineTarget(TestCase):
-
-    def test_subscribe_none(self):
-        target = CoRoutineTarget('hello', [], [])
-        listener = Listener(Queue())
-        target.subscribe(listener)
-        self.assertEqual({target.start_id}, set(listener._callbacks))
-
-    def test_subscribe_two(self):
-        dependencies = [Target('first', []), Target('second', [])]
-        target = CoRoutineTarget('hello', [], dependencies)
-        queue = Queue()
-        listener = Listener(queue)
-        target.subscribe(listener)
-        self.assertEqual({
-            dependencies[0].success_id,
-            dependencies[0].failure_id,
-            dependencies[1].success_id,
-            dependencies[1].failure_id,
-            target.start_id,
-        }, set(listener._callbacks))
-
-    def test_start(self):
-        output = StringIO()
-        dependencies = [Target('first', []), Target('second', [])]
-
-        def write_foo():
-            output.write('foo')
-
-        target = CoRoutineTarget('hello', [write_foo], dependencies)
-        events = list(target.start(Event(target.start_id)))
-        self.assertEqual(events, [])
-        events = list(target.start(Event(dependencies[0].success_id)))
-        self.assertEqual(events, [])
-        events = list(target.start(Event(dependencies[1].success_id)))
-        self.assertNotEqual(events, [])
-
-    def test_start_failure(self):
-        dependencies = [Target('first', []), Target('second', [])]
-        target = CoRoutineTarget('hello', [], dependencies)
-        events = list(target.start(Event(target.start_id)))
-        self.assertEqual(events, [])
-        (event,) = list(target.start(Event(dependencies[0].failure_id)))
-        self.assertEqual(('hello', 'failure'), event.event_id)
-        events = list(target.start(Event(dependencies[1].success_id)))
-        self.assertEqual(events, [])
-
-    def test_start_after_failure(self):
-        # Don't even report as failed unless we've tried to start it.
-        dependencies = [Target('first', []), Target('second', [])]
-        target = CoRoutineTarget('hello', [], dependencies)
-        events = list(target.start(Event(dependencies[0].failure_id)))
-        self.assertEqual([e.event_id for e in events], [target.failure_id])
-        events = list(target.start(Event(dependencies[1].success_id)))
-        self.assertEqual(events, [])
-        events = list(target.start(Event(target.start_id)))
-        self.assertEqual([], events)
-
-    def test_no_run_after_failure(self):
-
-        def fail():
-            raise Exception('I am exceptional!')
-
-        target = CoRoutineTarget('foo', [fail], [])
-        events = list(target.start(Event(target.start_id)))
-        self.assertNotEqual([], events)
-        events = list(target.start(Event(target.start_id)))
-        self.assertEqual([], events)
-
-    def test_no_run_after_success(self):
-        output = []
-
-        def add_output():
-            output.append('hello')
-
-        target = CoRoutineTarget('foo', [add_output], [])
-        events = list(target.start(Event(target.start_id)))
-        self.assertNotEqual([], events)
-        events = list(target.start(Event(target.start_id)))
-        self.assertEqual([], events)
-
-    def test_run(self):
-        output = StringIO()
-
-        def write_foo():
-            output.write('foo')
-
-        def write_bar():
-            output.write('bar')
-
-        foo_target = CoRoutineTarget('foo', [write_foo], [])
-        bar_target = CoRoutineTarget('bar', [write_bar], [foo_target])
         pipeline = Pipeline([bar_target, foo_target], bar_target)
         Listener(Queue()).run_pipeline(pipeline)
         self.assertEqual('foobar', output.getvalue())
